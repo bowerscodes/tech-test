9bb86b29d8d4e65c554f996c05380c787efadfc1
James Bowers <jamesbowers123@gmail.com>
2022-06-09 16:17:16 +0100

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/app.css';
 import Search from '../components/Search';
+import SearchResults from '../components/SearchResults';
 
 const App = () => {
-
+const [searchResults, setSearchResults] = useState();
   return (
     <div className="app">
-      <img src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg" className="app-logo" alt="nasaLogo" />
-      <Search />
+      <img
+      src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
+      className="app-logo"
+      alt="nasaLogo" />
+      <Search 
+        setSearchResults={setSearchResults}
+      />
+      <SearchResults />
     </div>
   );
 }
--- src/requests/getImages.js
+++ src/requests/getImages.js
@@ -6,9 +6,12 @@ function getImages(query) {
     .get(`https://images-api.nasa.gov/search?q=${query}`)
     .then((response) => {
       const imageResults = response.data.collection.items;
-      const parsedImages = imageResults.filter.data[0]("media_type" === "image");
+      // console.log(imageResults, 'IMAGERESULTS')
+      const parsedImages = imageResults.filter(image => image.data[0].media_type === 'image');
+      // console.log(parsedImages, 'PARSEDIMAGES')
       const images = parsedImages.map(image => image.links[0].href);
-      return images
+      // console.log(images, 'IMAGES')
+      return(images);
     })
     .catch((error) => {
       console.log(error);
